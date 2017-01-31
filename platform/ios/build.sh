c1448011e1afe63f2eefb0fdb45d222eb70a4f63
Masayuki Murata <muratams@jp.ibm.com>
2017-01-31 13:45:41 +0900

    update Podfile and build.sh in ios platform

--- platform/ios/build.sh
+++ platform/ios/build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-xcodebuild -workspace bleloc.xcworkspace -scheme bleloc -sdk iphoneos;
-xcodebuild -workspace bleloc.xcworkspace -scheme bleloc -sdk iphonesimulator;
-xcodebuild -workspace bleloc.xcworkspace -scheme framework;
-xcodebuild -workspace bleloc.xcworkspace -scheme bleloc -sdk iphoneos;
+
+if [ $# -eq 0 ]
+ then
+  CONFIG="Debug"
+elif [ $1 = "Release" -o $1 = "Debug" ]
+ then
+  CONFIG=$1
+else
+  echo "unknown configuration" $1
+  exit 1
+fi
+
+xcodebuild -configuration $CONFIG -workspace bleloc.xcworkspace -scheme bleloc -sdk iphoneos;
+xcodebuild -configuration $CONFIG -workspace bleloc.xcworkspace -scheme bleloc -sdk iphonesimulator;
+xcodebuild -configuration $CONFIG -workspace bleloc.xcworkspace -scheme framework;
+xcodebuild -configuration $CONFIG -workspace bleloc.xcworkspace -scheme bleloc -sdk iphoneos;
